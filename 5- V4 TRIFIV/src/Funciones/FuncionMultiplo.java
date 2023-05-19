de843a54ff006e04e2028844fc655edd7a76381b
LuisGR03 <candyofdeath27@gmail.com>
2023-05-19 11:20:44 -0600

    Se agrego Funcion Multiplo de version 4 TRIFIV

---
+++ 5- V4 TRIFIV/src/Funciones/FuncionMultiplo.java
@@ -0,0 +1,21 @@
+
+package Funciones;
+
+
+public class FuncionMultiplo {
+    
+    public String returnaMultiplo(int num){
+        
+        if(num == 0 && num == 0){
+                    return("TRIFIV");
+                    
+                } else if(num == 0){
+                    return("TRI");
+                    
+                }else if(num == 0){
+                    return("FIV");
+                }
+                return "Ningun valor";
+    }
+        
+    }
