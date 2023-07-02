bed8afa162451112ccd17dc13f224e6eefe1389a
LuisGR03 <candyofdeath27@gmail.com>
2023-07-02 17:41:14 -0600

    Se agrego Excepciones1

---
+++ Unidad 2/Excepcion1/src/unidad2/Unidad2.java
@@ -0,0 +1,21 @@
+
+package unidad2;
+
+import javax.swing.JOptionPane;
+
+public class Unidad2 {
+
+    public static void main(String[] args){
+        
+   int x, y;
+   
+   try{   
+   x= Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese numero 1:", "Solicito datos",3));
+   
+   }catch(NumberFormatException n){
+       
+       System.out.println(n.getMessage() + "No es numero Entero");
+   
+   } 
+}
+}
